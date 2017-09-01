4557ec45612cdb58c1eb83bae023e904498271fe
SadEvilPenguin <mears.chris@gmail.com>
2017-09-01 15:28:29 -0400

    javascript work

--- exercises/intro-to-javascript/test.js
+++ exercises/intro-to-javascript/test.js
@@ -1,1 +1,13 @@
 console.log("Hello, Chris!");
+var someArray = [1, 2, 3];
+
+var someFunc = function() {
+
+    for (let i = 0; i < someArray.length; i++) {
+        console.log(someArray[i]);
+    }
+};
+
+someArray.forEach((theThing) => {
+    console.logItheThing;
+});
