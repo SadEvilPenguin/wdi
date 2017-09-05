e03ac87fb5ae666293c46e7aa3f7dc611187fd9f
SadEvilPenguin <mears.chris@gmail.com>
2017-09-05 14:52:21 -0400

    For each loop practice in extras

---
+++ extra/forEach.js
@@ -0,0 +1,16 @@
+const array = [1, 2, 3, 4, 5];
+//old way
+for (var i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
+//new way
+//fake variable for each index in this case. Similar to i in the above for loop
+array.forEach(function(number) {
+    console.log(number);
+});
+
+//can also create function first then call with array
+const logTheNumber = function(number) {
+    console.log(number);
+}
+array.forEach(logTheNumber);
